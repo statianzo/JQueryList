08017d429fac63931b4bf92b5500cf2d926433bc
Jason Staten <jstaten07@gmail.com>
2011-09-05 00:18:54 -0600

    Added stats

--- script/app.js
+++ script/app.js
@@ -1,4 +1,29 @@
 (function(){
+  function updateStats(){
+    if ($('.todo').length){
+      $('#todo-stats').show();
+      var remaining = $('.todo:not(.complete)');
+      $('#remaining-count').text(remaining.length);
+      $('#remaining-count-word').text(remaining.length === 1 ? 'item' : 'items');
+      
+      var complete = $('.todo.complete');
+      if (complete.length){
+        $('.todo-clear').show();
+        $('#completed-count').text(complete.length);
+        $('#completed-count-word').text(complete.length === 1 ? 'item' : 'items');
+        $('.todo-clear a').click(function(){
+          complete.remove();
+          updateStats();
+        });
+      }
+      else {
+        $('.todo-clear').hide();
+      }
+    }
+    else {
+      $('#todo-stats').hide();
+    }
+  }
   $('#create-todo').submit(function(e){
     e.preventDefault();
     var item = $('#viewItem')
@@ -7,18 +32,21 @@
 
     //Complete items
     $('.check', item).click(function(){
-      if($(this).is(':checked')){
+      if ($(this).is(':checked')) {
         item.addClass('complete');
       }
       else {
         item.removeClass('complete');
       }
+      updateStats();
     });
 
     //Remove items
     $('.todo-destroy',item).click(function(){
       item.remove();
+      updateStats();
     });
     $('#new-todo').val('');
+    updateStats();
   });
 }());
