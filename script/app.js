31054ae229b2ef5542b8ef51d95c7a8114c499a9
Jason Staten <jstaten07@gmail.com>
2011-09-05 00:02:35 -0600

    Complete and remove items

--- script/app.js
+++ script/app.js
@@ -1,9 +1,24 @@
 (function(){
   $('#create-todo').submit(function(e){
     e.preventDefault();
-    $('#viewItem')
+    var item = $('#viewItem')
       .tmpl({text: $('#new-todo').val(), complete:false})
       .appendTo('#todo-list');
+
+    //Complete items
+    $('.check', item).click(function(){
+      if($(this).is(':checked')){
+        item.addClass('complete');
+      }
+      else {
+        item.removeClass('complete');
+      }
+    });
+
+    //Remove items
+    $('.todo-destroy',item).click(function(){
+      item.remove();
+    });
     $('#new-todo').val('');
   });
 }());
