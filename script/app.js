a12b3cb583514e291ae29c740496eeae44294645
Jason Staten <jstaten07@gmail.com>
2011-09-04 23:52:18 -0600

    adds items on enter

--- script/app.js
+++ script/app.js
@@ -1,3 +1,9 @@
 (function(){
-  //Code Here
+  $('#create-todo').submit(function(e){
+    e.preventDefault();
+    $('#viewItem')
+      .tmpl({text: $('#new-todo').val(), complete:false})
+      .appendTo('#todo-list');
+    $('#new-todo').val('');
+  });
 }());
